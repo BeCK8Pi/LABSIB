3c90f8b8d3201ee57415e416af958d49e2adabdb
BeCK8Pi <khuntip1207@gmail.com>
2023-12-19 16:04:06 +0700

    fisinh

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    ifstream source;
+    int number=0;
+    double mean=0, stan=0, num;
+    string read;
+
+    source.open("D:\\Git\\LABSIB\\score.txt");
+    while(getline(source,read)){
+        number += 1;
+        num = atof(read.c_str());
+        mean += num;
+        stan += pow(num,2);
+    }
+    mean = mean/number;
+    stan = sqrt(stan/number-pow(mean,2));
+    source.close();
+
+    cout << "Number of data = " << number << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << mean << endl;
+    cout << "Standard deviation = " << stan << endl;
+}
